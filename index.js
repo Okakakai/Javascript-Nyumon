f8caf8982beec8d39b7b3a3700458f5392267592
Okakakai <kai48586@gmail.com>
2022-02-08 22:17:29 +0900

    あきた.com

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function add(todo) {
 }
 
 function saveData() {
-    const lists = document.querySelectorAll("li");
+    const lists = document.querySelectorAll("#ul .ul_li");
     let todos = [];
     lists.forEach(list => {
         let todo = {
@@ -66,3 +66,13 @@ function saveData() {
     });
     localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+function hamburger() {
+    document.getElementById('line1').classList.toggle('line_1');
+    document.getElementById('line2').classList.toggle('line_2');
+    document.getElementById('line3').classList.toggle('line_3');
+    document.getElementById('nav').classList.toggle('in');
+}
+document.getElementById('hamburger').addEventListener('click', function() {
+    hamburger();
+});
